da25b17757f23da4bea041365308e38c5063b8c2
Chris Daniells <chrisdaniells@hotmail.co.uk>
2018-01-24 13:06:54 +0000

    basic search and results styling

--- src/js/Components/SearchBox.js
+++ src/js/Components/SearchBox.js
@@ -6,11 +6,18 @@ export default class SearchContainer extends React.Component {
             <div>
                 <h1>Nasa Search</h1>
                 <div className="c-searchbox">
-                    <input type="text" className="c-searchbox__input" />
-                    <button className="c-searchbox__button">Search</button>
+                    <input type="text" className="c-searchbox__input" placeholder="Enter Search Term for Lift Off"/>
+                    <button className="c-searchbox__button">
+                        <img src="https://nasa.gov/sites/all/themes/custom/nasatwo/images/search.svg"
+                            className="c-searchbox__button-icon" />
+                    </button>
                     <div className="c-searchbox__filter">
-                        <label><input type="checkbox" className="c-searchbox__checkbox" />Images</label>
-                        <label><input type="checkbox" className="c-searchbox__checkbox" />Audio</label>
+                        <label className="c-searchbox__label">
+                            <input type="checkbox" className="c-searchbox__checkbox" />Images
+                        </label>
+                        <label className="c-searchbox__label">
+                            <input type="checkbox" className="c-searchbox__checkbox" />Audio
+                        </label>
                     </div>
                 </div>
 
--- src/js/Components/SearchResults.js
+++ src/js/Components/SearchResults.js
@@ -4,11 +4,29 @@ export default class SearchContainer extends React.Component {
     render() {
         return (
             <div>
-                <h1>Nasa Search</h1>
-                <div className="c-results">
-                    <div className="c-results__item">
+                <div className="c-searchresults">
+                    <div className="c-searchresults__item">
                         <img src="http://via.placeholder.com/200x200" />
                     </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    <div className="c-searchresults__item">
+                        <img src="http://via.placeholder.com/200x200" />
+                    </div>
+                    
                 </div>
 
             </div>
