a1ce146819a29579ccd4441501cbca20b2a1dc2e
Chris Daniells <chrisdaniells@hotmail.co.uk>
2018-01-24 18:29:36 +0000

    Set asset data to veriables instead of static strings

--- src/js/Components/AssetItem.js
+++ src/js/Components/AssetItem.js
@@ -4,8 +4,8 @@ export default class AssetItem extends React.Component {
     render() {
         return(
             <div className="c-asset">
-                <h1 className="c-asset__title">this.props.title</h1>
-                <p className="c-asset__description">this.props.description</p>
+                <h1 className="c-asset__title">{this.props.title}</h1>
+                <p className="c-asset__description">{this.props.description}</p>
                 <img src={this.props.imagePath} className="c-asset__image" />
             </div>
         )
