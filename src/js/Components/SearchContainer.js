e9cb33b854d8cdba42b5f21fa172c9e6504c7cbb
Chris Daniells <chrisdaniells@hotmail.co.uk>
2018-01-24 11:18:37 +0000

    get react router set up to use hash history for static spa

--- src/js/Components/SearchContainer.js
+++ src/js/Components/SearchContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class SearchContainer extends React.Component {
     render() {
         return (
-            <div>SearchContainer</div>
+            <div>
+                <p>SearchContainer</p>
+                <Link to="/search">Search</Link>
+            </div>
         )
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import AssetContainer from './Components/AssetContainer';
 import SearchContainer from './Components/SearchContainer';
