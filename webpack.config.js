d8b6519c09eded8cadf8b77fc61e7986186dd9c6
Chris Daniells <chrisdaniells@hotmail.co.uk>
2018-01-24 10:16:51 +0000

    Set up app environment

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const PATHS = {
+    app: './src/js/app.js',
+    dist: path.join(__dirname, './dist/js')
+};
+
+module.exports = {
+    devtool: 'inline-sourcemap',
+    context: __dirname,
+    entry: PATHS.app,
+    output: {
+        path:  PATHS.dist,
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader" 
+            }
+        ]
+    }
+};
