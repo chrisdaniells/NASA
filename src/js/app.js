3132850e9408d1b018bd6c3731133543b24401a5
Chris Atkin <atkinchris@gmail.com>
2018-02-06 12:19:12 +0000

    Add webpack dev server

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import React from 'react'
+import { render } from 'react-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 
-import AssetContainer from './Components/AssetContainer';
-import SearchContainer from './Components/SearchContainer';
+import AssetContainer from './Components/AssetContainer'
+import SearchContainer from './Components/SearchContainer'
 
-const history = createBrowserHistory();
+import '../scss/app.scss'
 
-render (
-    <Router>
-        <Switch>
-            <Route path="/search/:query?/:assettype?" component={SearchContainer} />
-            <Route path="/asset/:type/:nasaid" component={AssetContainer} />
-            <Route exact path="/" component={SearchContainer} />
-        </Switch>
-    </Router>,
-    document.getElementById('root')
+const history = createBrowserHistory()
+
+render(
+  <Router>
+    <Switch>
+      <Route path="/search/:query?/:assettype?" component={SearchContainer} />
+      <Route path="/asset/:type/:nasaid" component={AssetContainer} />
+      <Route exact path="/" component={SearchContainer} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'),
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,56 @@
-var webpack = require('webpack');
-var path = require('path');
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const PATHS = {
-    app: './src/js/app.js',
-    dist: path.join(__dirname, './dist/js')
-};
+const paths = {
+  SRC: path.resolve(__dirname, 'src'),
+  DEST: path.resolve(__dirname, 'dist'),
+}
 
-module.exports = {
-    devtool: 'inline-sourcemap',
-    context: __dirname,
-    entry: ['whatwg-fetch', PATHS.app],
-    output: {
-        path: PATHS.dist,
-        filename: 'app.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
-    }
-};
+const config = {
+  entry: {
+    main: paths.SRC,
+  },
+  output: {
+    path: paths.DEST,
+    publicPath: '',
+    filename: '[name].[hash:8].js',
+    chunkFilename: '[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
+    }, {
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader"
+      }, {
+          loader: "css-loader"
+      }, {
+          loader: "sass-loader"
+      }]
+  }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].[contenthash:8].css'),
+    new HtmlWebpackPlugin({ template: path.join(paths.SRC, 'index.html') }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+    new webpack.NamedModulesPlugin(),
+  ],
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    stats: 'errors-only',
+  },
+}
+
+module.exports = config
